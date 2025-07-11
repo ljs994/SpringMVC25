a179bb76541fc9a86898d1b568d6036409dfe9b5
220-27\dlwotkd <ysy06022@gmail.com>
2025-07-11 10:06:56 +0900

    마이바티스xml로 테스트 완료(CRUD)

---
+++ src/main/java/org/mbc/domain/BoardVO.java
@@ -0,0 +1,20 @@
+package org.mbc.domain;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data	// import lombok.Data;, (생성자, 게터/세터/ to String등...)
+public class BoardVO {
+	// 객체용으로 vo는 읽기전용 속성이 강하게 만드는 객체(게터 위주로 셋팅)
+	// dto는 게터/세터 모두 가지고 활용하는 객체
+	
+	private Long bno;
+	private String title;
+	private String content;
+	private String writer;
+	private Date regDate;	// import java.util.Date;
+	private Date updateDate;
+	
+
+}
